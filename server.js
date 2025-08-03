b324a1c955a9394ad39a08fe104987a0fd8183c6
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-08-03 14:53:50 +0200

    add server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(8000, () => {
+  console.log('Server is running');
+});
