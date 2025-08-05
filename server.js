2e2039a345da0983074f9244e3651591240fc2ff
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-08-05 12:15:20 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -12,10 +13,15 @@ app.use(express.json());
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: '404 not found...' });
 });
 
-app.listen(8000, () => {
-  console.log('Server is running');
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
