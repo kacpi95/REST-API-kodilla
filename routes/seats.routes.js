5fc92a1f3fdb4c3bfcd2a024edeaa40051afb090
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-09-23 18:22:33 +0200

    refactor: seats.routes

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,63 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
-const shortid = require('shortid');
+const SeatController = require('../controllers/seats.controller');
 
-router.route('/seats').get((req, res) => {
-  res.json(db.seats);
-});
+router.get('/seats', SeatController.getAll);
+router.get('/seats/random', SeatController.getRandom);
+router.get('/seats/:id', SeatController.getSeatId);
 
-router.get('/seats', (req, res) => {
-  res.json(db.seats);
-});
-router.get('/seats/random', (req, res) => {
-  const random = db.seats[Math.floor(Math.random() * db.seats.length)];
-  res.json(random);
-});
-router.get('/seats/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const testimonial = db.seats.find((t) => t.id === id);
-  res.json(testimonial);
-});
-
-router.post('/seats', (req, res) => {
-  console.log('Request body:', req.body);
-
-  const { day, seat, client, email } = req.body;
-  if (!day || !seat || !client || !email) {
-    return res.status(404).json({ message: 'Complete the fields' });
-  }
-  const checkSeats = db.seats.some((e) => e.day === day && e.seat === seat);
-  if (checkSeats) {
-    return res.status(400).json({ message: 'The slot is already taken...' });
-  }
-
-  const newElement = { id: shortid.generate(), day, seat, client, email };
-  db.seats.push(newElement);
-
-  req.io.emit('seatsUpdated', db.seats);
-  res.json({ message: 'OK' });
-});
-router.put('/seats/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const { author, text } = req.body;
-  const testimonialId = db.seats.findIndex((t) => t.id === id);
-  if (testimonialId !== -1) {
-    db.seats[testimonialId] = { id, author, text };
-    res.json({ message: 'OK' });
-  } else {
-    res.status(404).json({ message: 'Error' });
-  }
-});
-router.delete('/seats/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const testimonialId = db.seats.findIndex((t) => t.id === id);
-  if (testimonialId !== -1) {
-    db.seats.splice(testimonialId, 1);
-    res.json({ message: 'OK' });
-  } else {
-    res.status(404).json({ message: 'Error' });
-  }
-});
+router.post('/seats', SeatController.postSeat);
+router.put('/seats/:id', SeatController.putSeatId);
+router.delete('/seats/:id', SeatController.deleteSeat);
 
 module.exports = router;
