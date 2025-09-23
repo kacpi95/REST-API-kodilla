c66411dd573fd647c06cc4528ad70d9e3fb80d46
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-09-23 17:25:45 +0200

    refactio: method GET in testimonials

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -2,23 +2,40 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 const shortid = require('shortid');
+const Testimonial = require('../models/testimonials.model');
 
 router.route('/testimonials').get((req, res) => {
   res.json(db.testimonials);
 });
 
-router.get('/testimonials', (req, res) => {
-  res.json(db.testimonials);
+router.get('/testimonials', async (req, res) => {
+  try {
+    res.json(Testimonial.find());
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
-router.get('/testimonials/random', (req, res) => {
-  const random =
-    db.testimonials[Math.floor(Math.random() * db.testimonials.length)];
-  res.json(random);
+
+router.get('/testimonials/random', async (req, res) => {
+  try {
+    const count = await Testimonial.countDocuments();
+    const random = Math.floor(Math.random() * count);
+    const newTestimonial = await Testimonial.findOne().skip(random);
+    if (!newTestimonial) res.status(404).json({ message: 'Not Found' });
+    else res.json({ message: 'OK' });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
-router.get('/testimonials/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const testimonial = db.testimonials.find((t) => t.id === id);
-  res.json(testimonial);
+
+router.get('/testimonials/:id', async (req, res) => {
+  try {
+    const testimonial = await Testimonial.findById(req.params.id);
+    if (!testimonial) res.status(404).json({ message: 'Not Found' });
+    else res.json(testimonial);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
 
 router.post('/testimonials', (req, res) => {
