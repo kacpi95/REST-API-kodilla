5e8bb61ff0bbe2a63f5b9e5afcb3eae947d751a0
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-09-23 16:42:38 +0200

    update: testimonials model

--- models/testimonials.model.js
+++ models/testimonials.model.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 
 const testimonialSchema = new mongoose.Schema({
   author: { type: String, require: true },
+  text: { type: String, require: true },
 });
 module.exports = mongoose.model('Testimonial', testimonialSchema);
