b317737486c4bd40a086d5179ffe03910d43e8ab
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2026-02-06 17:21:06 +0100

    update: all styles in file styles

--- client/src/components/layout/MainLayout/MainLayout.jsx
+++ client/src/components/layout/MainLayout/MainLayout.jsx
@@ -3,10 +3,10 @@ import Footer from '../Footer/Footer';
 
 export default function MainLayout({ children }) {
   return (
-    <>
+    <div className='layout'>
       <MainMenu />
-      <main>{children}</main>
+      <main className='content'>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
